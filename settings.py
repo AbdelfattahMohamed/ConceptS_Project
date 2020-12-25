d8ee2f75037f68633b5321ca934cd3e54f5252a9
Abdelrhman abduulah <abdonilsh20@fci.helwan.edu.eg>
2020-12-25 19:58:58 +0200

    Creating start menu and griding the maze

---
+++ settings.py
@@ -0,0 +1,19 @@
+
+
+
+#screen settings
+WIDTH , HEIGHT =    560 , 620
+FPS = 60 
+
+
+#color settings
+BLACK = (0,0,0)
+RED = (208,22,22)
+GREY = (107,107,107)
+
+#font settings
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+#player settings 
+
+#mob settings
